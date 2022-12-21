a89900eaf35dcd865393cff25caaedf1f6402cb4
Pavlo Skoreiko <pavelroberts@gmail.com>
2022-12-21 11:31:49 +0100

    picture on click

---
+++ inne/dff.js
@@ -0,0 +1,38 @@
+console.log("Cześć! ");
+
+
+let select = document.querySelector("select");
+let html = document.querySelector("html");
+document.body.style.padding = "10px";
+
+function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+}
+
+select.onchange = function () {
+  (select.value === "black") ? update("black", "white") : update("white", "black");
+}
+
+
+let button = document.querySelector(".js-button");
+let image = document.querySelector(".js-image");
+let anotherName = document.querySelector(".js-anotherName");
+
+
+
+button.addEventListener("click", () => {
+  switch (image.alt) {
+    case "Święty Mikołaj":
+      image.src = "https://i.postimg.cc/QdVvnjF1/PIK4301.jpg";
+      image.alt = "Elfy";
+      anotherName.innerText = "Mikolaj";
+      break;
+
+    case "Elfy":
+      image.src = "https://i.postimg.cc/BbFqXf3r/Mikolaj.jpg";
+      image.alt = "Święty Mikołaj";
+      anotherName.innerText = "elfy";
+      break;
+  }
+})
